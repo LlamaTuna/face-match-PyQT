fb393c6a3c73fb3f73c36e392779891932072ce3
Jeff Becker <snarkvader@protonmail.com>
2023-04-25 04:05:03 -0700

    + input image conversion to png error and handling

--- face_detection.py
+++ face_detection.py
@@ -7,9 +7,17 @@ import face_recognition
 input_folder = "./faceTests/"
 output_folder = "./output/"
 
+def convert_image_format(image_path, output_format='png'):
+    img = cv2.imread(image_path)
+    if img is None:
+        raise ValueError(f"Unable to read image from {image_path}")
+    output_path = os.path.splitext(image_path)[0] + '.' + output_format
+    cv2.imwrite(output_path, img)
+    return output_path
+
 def save_faces_from_folder(folder_path, face_cascade, output_folder, progress_callback=None):
     face_data = {}
-    valid_extensions = ['.png', '.jpeg', '.jpg']
+    valid_extensions = ['.png', '.jpeg', '.jpg', '.bmp']
 
     image_names = os.listdir(folder_path)
     num_images = len(image_names)
@@ -21,39 +29,49 @@ def save_faces_from_folder(folder_path, face_cascade, output_folder, progress_ca
             continue
 
         image_path = os.path.join(folder_path, image_name)
-        img = cv2.imread(image_path)
-        faces = face_recognition.face_locations(img)
 
-        if len(faces) > 0:
-            img_hash = hashlib.sha256(open(image_path, 'rb').read()).hexdigest()
-            face_data[img_hash] = {"file_name": image_name, "faces": []}  # Store the original image name
-            for (top, right, bottom, left) in faces:
-                face_img = img[top:bottom, left:right]
-                face_data[img_hash]["faces"].append(face_img)
-                output_path = os.path.join(output_folder, f"{img_hash}_{len(face_data[img_hash]['faces'])}.png")
-                cv2.imwrite(output_path, face_img)
+        try:
+            converted_image_path = convert_image_format(image_path, output_format='png')
+            img = cv2.imread(converted_image_path)
+            assert img is not None, f"Image at {converted_image_path} is None"
 
-        if progress_callback:
-            progress_callback(idx / num_images * 100)
+            faces = face_recognition.face_locations(img)
+
+            if len(faces) > 0:
+                img_hash = hashlib.sha256(open(converted_image_path, 'rb').read()).hexdigest()
+                face_data[img_hash] = {"file_name": image_name, "faces": []}
+                for (top, right, bottom, left) in faces:
+                    face_img = img[top:bottom, left:right]
+                    face_data[img_hash]["faces"].append(face_img)
+                    output_path = os.path.join(output_folder, f"{img_hash}_{len(face_data[img_hash]['faces'])}.png")
+                    cv2.imwrite(output_path, face_img)
+
+            if progress_callback:
+                progress_callback(idx / num_images * 100)
+
+        except Exception as e:
+            print(f"Error processing image {image_name}: {e}")
 
     return face_data
 
 def find_matching_face(image_path, face_data, threshold=0.5):
-
     img = cv2.imread(image_path)
-    faces = face_recognition.face_locations(img)
+    if img is None:
+        raise ValueError(f"Unable to read image from {image_path}")
 
+    faces = face_recognition.face_locations(img)
     matching_faces = []
 
     for (top, right, bottom, left) in faces:
         face_img = img[top:bottom, left:right]
         face_img = cv2.resize(face_img, (100, 100))
         for img_hash, stored_data in face_data.items():
-            stored_faces = stored_data["faces"]  # Access the stored faces
+            stored_faces = stored_data["faces"]
             for i, stored_face in enumerate(stored_faces):
                 stored_face_resized = cv2.resize(stored_face, (100, 100))
                 similarity = np.mean(np.abs(face_img.astype(np.float32) - stored_face_resized.astype(np.float32))) / 255.0
 
                 if similarity < threshold:
-                    matching_faces.append((img_hash, stored_data["file_name"], stored_face, similarity, f"{img_hash}_{i+1}.png"))  # Add the resized image name
+                    matching_faces.append((img_hash, stored_data["file_name"], stored_face, similarity, f"{img_hash}_{i+1}.png"))
+
     return matching_faces
